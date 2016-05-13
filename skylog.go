1a40e89395cefa614347e65c07cb86689343088c
Alex Cornford <acornford@Alexs-MacBook-Pro.local>
2016-05-13 16:10:12 -0400

    First Code Commit, there is still much work to be done

---
+++ skylog.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"io"
+
+	//"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+)
+
+type log_event struct {
+	location  string
+	message   string
+	timestamp int64
+}
+
+func LogReader(log_file string, log_channel chan log_event) {
+	file, _ := os.Open(log_file)
+	file_reader := bufio.NewReader(file)
+	for {
+		line, _, err := file_reader.ReadLine()
+		if err == io.EOF {
+			time.Sleep(10)
+		} else {
+			log_channel <- log_event{
+				location:  log_file,
+				message:   string(line),
+				timestamp: time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+			}
+		}
+	}
+}
+
+func LogSender(log_channel chan log_event) {
+	var (
+		nextSeqToken string
+		credential   *credentials.Credentials
+		logEvents    []*cloudwatchlogs.InputLogEvent
+	)
+
+	logGroupName := "test"
+	logStreamName := "alexs-mbp"
+
+	aws_config := aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credential}
+
+	sess := session.New(&aws_config)
+	svc := cloudwatchlogs.New(sess)
+	checkTime := time.Now().Unix()
+
+	for {
+		select {
+		case elm, status := <-log_channel:
+			if status {
+				//send every 100 log events, or every 5 seconds
+				if len(logEvents) < 100 || time.Now().Unix()-checkTime < 2 {
+					logEvents = append(logEvents, &cloudwatchlogs.InputLogEvent{
+						Timestamp: aws.Int64(elm.timestamp), Message: &elm.message})
+				} else {
+					if nextSeqToken == "" {
+						eventInput := cloudwatchlogs.PutLogEventsInput{
+							LogGroupName:  &logGroupName,
+							LogStreamName: &logStreamName,
+							LogEvents:     logEvents,
+						}
+						resp, err := svc.PutLogEvents(&eventInput)
+						if err != nil {
+							fmt.Println(err)
+							errorString := strings.Split(string(err.Error()), " ")
+							nextSeqToken = strings.Split(errorString[len(errorString)-6], "\n")[0]
+							fmt.Println(nextSeqToken)
+							fmt.Println("DONE")
+						}
+						if resp == nil {
+						}
+					} else {
+						eventInput := cloudwatchlogs.PutLogEventsInput{
+							LogGroupName:  &logGroupName,
+							LogStreamName: &logStreamName,
+							LogEvents:     logEvents,
+							SequenceToken: &nextSeqToken,
+						}
+						fmt.Println(eventInput)
+						resp, err := svc.PutLogEvents(&eventInput)
+						nextSeqToken = *resp.NextSequenceToken
+						fmt.Println("Success", resp)
+						if err != nil {
+							fmt.Println(err)
+							fmt.Println(resp.GoString())
+						}
+					}
+					checkTime = time.Now().Unix()
+					logEvents = logEvents[:0]
+				}
+			}
+		default:
+			time.Sleep(10)
+		}
+	}
+}
+
+func main() {
+
+	runtime.GOMAXPROCS(2)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	file_channel := make(chan log_event, 1000000)
+	go LogReader("/var/log/system.log", file_channel)
+	go LogSender(file_channel)
+	wg.Wait()
+}
